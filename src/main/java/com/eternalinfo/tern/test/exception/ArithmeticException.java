a9ba97c9014ba0ea5a4f3fb5876602bda8f12163
王诚沣 <920465415@qq.com>
2020-02-11 13:13:55 +0800

    算子引擎新增异常类

---
+++ src/main/java/com/eternalinfo/tern/test/exception/ArithmeticException.java
@@ -0,0 +1,19 @@
+package com.eternalinfo.tern.test.exception;
+
+/**
+ * @author 王诚沣
+ * @下午1:01:50
+ * @description
+ * @version
+ */
+public class ArithmeticException extends  Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6576746325878802869L;
+	
+	public ArithmeticException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/eternalinfo/tern/test/exception/ExecuteException.java
@@ -0,0 +1,20 @@
+package com.eternalinfo.tern.test.exception;
+
+/**
+ * @author 王诚沣
+ * @下午1:04:14
+ * @description
+ * @version
+ */
+public class ExecuteException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2731385578123596407L;
+	
+	public ExecuteException(String message) {
+		super(message);
+	}
+	
+}
