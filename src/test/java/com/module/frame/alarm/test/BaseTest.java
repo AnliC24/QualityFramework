9146780596d7c1a5db1fe7dfe45f5fb0adffa625
王诚沣 <920465415@qq.com>
2020-02-11 15:57:07 +0800

    新增测试用例基类

---
+++ src/test/java/com/module/frame/alarm/test/BaseTest.java
@@ -0,0 +1,14 @@
+package com.module.frame.alarm.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author 王诚沣
+ * @下午3:53:41
+ * @description
+ * @version
+ */
+public abstract class BaseTest {
+	protected  Logger LOG = LogManager.getLogger(this.getClass());
+}
